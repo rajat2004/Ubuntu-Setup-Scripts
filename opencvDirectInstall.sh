d9a928db4d3e1b95f53bad56b02152742c80657c
Rajat Singhal <rajatsinghal564@gmail.com>
2020-01-02 15:39:40 +0530

    Add FFmpeg libs to dynamic linker

--- opencvDirectInstall.sh
+++ opencvDirectInstall.sh
@@ -119,6 +119,9 @@ if [[ ! -n $(cat $SHELLRC | grep '# ffmpeg-build-script') ]]; then
         if [[ ! -n $CIINSTALL ]]; then
             su - $USER
         fi
+
+        sudo sh -c 'echo "/opt/ffmpeg-build-script/workspace/lib" > /etc/ld.so.conf.d/ffmpeg.conf'
+        sudo ldconfig
     )
 fi
 
