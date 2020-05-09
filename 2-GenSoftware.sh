ad22ade9ad9b57cc6a341594d03ffcf3dfc5e51b
Rajat Singhal <rajatsinghal564@gmail.com>
2020-05-09 07:06:46 +0530

    Change to indicator-cpufreq

--- 2-GenSoftware.sh
+++ 2-GenSoftware.sh
@@ -89,7 +89,7 @@ cp ./config_files/tilda.desktop ~/.config/autostart
 cp ./config_files/redshift-gtk.desktop ~/.config/autostart
 
 execute sudo apt-get install htop cpufrequtils indicator-cpufreq gparted expect -y
-execute /usr/bin/cpufreq-info
+execute /usr/bin/indicator-cpufreq
 sudo sed -i 's/^GOVERNOR=.*/GOVERNOR=”powersave”/' /etc/init.d/cpufrequtils
 
 # Meld - Visual diff and merge tool
